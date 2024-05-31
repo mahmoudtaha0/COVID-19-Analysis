786e2bae963da0f8537512cccb3c74715597be16
mahmoudtaha0 <mahmoud.ahmed021@eng-st.cu.edu.eg>
2024-05-31 10:45:14 +0300

    Some monthly analysis

--- Analysis.py
+++ Analysis.py
@@ -5,6 +5,9 @@ import pandas as pd
 # Read data file
 df = pd.read_csv('COVID-19 Dataset.csv')
 
+# Convert Date from DD-MM-YYYY to YYYY-MM-DD
+df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y').dt.strftime('%Y-%m-%d')
+
 # Create an SQLite database
 conn = sqlite3.connect('covid.db')
 
@@ -59,7 +62,32 @@ print(count)
 results = pd.read_sql_query(nullValues, conn)
 print(results)
 
+# start_date and end_date
+dates = """
+SELECT MIN(Date) AS start_date, MAX(Date) AS end_date FROM covid_pandemic
+"""
+Dates = pd.read_sql_query(dates, conn)
+print(Dates)
 
+months = """
+SELECT COUNT(DISTINCT strftime('%Y-%m', Date)) AS number_of_months FROM covid_pandemic
+"""
+number_of_months = pd.read_sql_query(months, conn)
+print(number_of_months)
+
+# Monthly average for confirmed, deaths, and recovered
+monthly_averages_query = """
+SELECT strftime('%Y-%m', Date) AS MonthYear,
+AVG(Confirmed) AS average_confirmed,
+AVG(Deaths) AS average_deaths,
+AVG(Recovered) AS average_recovered
+FROM covid_pandemic
+GROUP BY MonthYear
+ORDER BY MonthYear;
+"""
+
+monthly_averages = pd.read_sql_query(monthly_averages_query, conn)
+print(monthly_averages)
 
 # Close connection
 # connection.close()
